f0ce5513b434297c5207dbf49dc57ae4be0ee6eb
KUIPERS Lolke <lolke.kuipers@altran.com>
2020-03-17 16:11:33 +0100

    add command box

--- PlotterComm/Form1.Designer.cs
+++ PlotterComm/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ivTbCommand = new System.Windows.Forms.TextBox();
+            this.ivBtnCommand = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ivTbCommand
+            // 
+            this.ivTbCommand.Location = new System.Drawing.Point(44, 62);
+            this.ivTbCommand.Name = "ivTbCommand";
+            this.ivTbCommand.Size = new System.Drawing.Size(442, 22);
+            this.ivTbCommand.TabIndex = 0;
+            // 
+            // ivBtnCommand
+            // 
+            this.ivBtnCommand.Location = new System.Drawing.Point(529, 61);
+            this.ivBtnCommand.Name = "ivBtnCommand";
+            this.ivBtnCommand.Size = new System.Drawing.Size(75, 23);
+            this.ivBtnCommand.TabIndex = 1;
+            this.ivBtnCommand.Text = "Command";
+            this.ivBtnCommand.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ivBtnCommand);
+            this.Controls.Add(this.ivTbCommand);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ivTbCommand;
+        private System.Windows.Forms.Button ivBtnCommand;
     }
 }
 
