81b7634f58dd6cd562a3c8011e78ea50a8a67277
KUIPERS Lolke <lolke.kuipers@altran.com>
2020-03-29 16:05:10 +0200

    serial

--- PlotterComm/Form1.cs
+++ PlotterComm/Form1.cs
@@ -21,5 +21,29 @@ namespace PlotterComm
 
             ivCbPorts.Items.AddRange(ports);
         }
+
+        private void ivBtnCommand_Click(object sender, EventArgs e)
+        {
+            var _serialPort = new SerialPort();
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = ivCbPorts.SelectedText;
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+
+            _serialPort.Write(ivTbCommand.Text + "\r\n");
+
+            _serialPort.Close();
+
+        }
     }
 }
