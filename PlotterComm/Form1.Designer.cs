a228af42dfae4018fd94fe941a2f345c70a9b626
KUIPERS Lolke <lolke.kuipers@altran.com>
2020-03-30 12:14:46 +0200

    CAN

--- PlotterComm/Form1.Designer.cs
+++ PlotterComm/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.ivTbCommand = new System.Windows.Forms.TextBox();
             this.ivBtnCommand = new System.Windows.Forms.Button();
             this.ivCbPorts = new System.Windows.Forms.ComboBox();
+            this.ivBtnConnect = new System.Windows.Forms.Button();
+            this.ivBtnDisconnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ivBtnCAN = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ivTbCommand
@@ -44,10 +48,11 @@
             // 
             this.ivBtnCommand.Location = new System.Drawing.Point(529, 61);
             this.ivBtnCommand.Name = "ivBtnCommand";
-            this.ivBtnCommand.Size = new System.Drawing.Size(75, 23);
+            this.ivBtnCommand.Size = new System.Drawing.Size(81, 23);
             this.ivBtnCommand.TabIndex = 1;
             this.ivBtnCommand.Text = "Command";
             this.ivBtnCommand.UseVisualStyleBackColor = true;
+            this.ivBtnCommand.Click += new System.EventHandler(this.ivBtnCommand_Click);
             // 
             // ivCbPorts
             // 
@@ -57,11 +62,53 @@
             this.ivCbPorts.Size = new System.Drawing.Size(121, 24);
             this.ivCbPorts.TabIndex = 2;
             // 
+            // ivBtnConnect
+            // 
+            this.ivBtnConnect.Location = new System.Drawing.Point(189, 13);
+            this.ivBtnConnect.Name = "ivBtnConnect";
+            this.ivBtnConnect.Size = new System.Drawing.Size(75, 23);
+            this.ivBtnConnect.TabIndex = 3;
+            this.ivBtnConnect.Text = "Connect";
+            this.ivBtnConnect.UseVisualStyleBackColor = true;
+            this.ivBtnConnect.Click += new System.EventHandler(this.ivBtnConnect_Click);
+            // 
+            // ivBtnDisconnect
+            // 
+            this.ivBtnDisconnect.Location = new System.Drawing.Point(297, 13);
+            this.ivBtnDisconnect.Name = "ivBtnDisconnect";
+            this.ivBtnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.ivBtnDisconnect.TabIndex = 4;
+            this.ivBtnDisconnect.Text = "Disconnect";
+            this.ivBtnDisconnect.UseVisualStyleBackColor = true;
+            this.ivBtnDisconnect.Click += new System.EventHandler(this.ivBtnDisconnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 172);
+            this.textBox1.TabIndex = 5;
+            // 
+            // ivBtnCAN
+            // 
+            this.ivBtnCAN.Location = new System.Drawing.Point(529, 101);
+            this.ivBtnCAN.Name = "ivBtnCAN";
+            this.ivBtnCAN.Size = new System.Drawing.Size(81, 23);
+            this.ivBtnCAN.TabIndex = 6;
+            this.ivBtnCAN.Text = "CAN";
+            this.ivBtnCAN.UseVisualStyleBackColor = true;
+            this.ivBtnCAN.Click += new System.EventHandler(this.ivBtnCAN_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ivBtnCAN);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ivBtnDisconnect);
+            this.Controls.Add(this.ivBtnConnect);
             this.Controls.Add(this.ivCbPorts);
             this.Controls.Add(this.ivBtnCommand);
             this.Controls.Add(this.ivTbCommand);
@@ -77,6 +124,10 @@
         private System.Windows.Forms.TextBox ivTbCommand;
         private System.Windows.Forms.Button ivBtnCommand;
         private System.Windows.Forms.ComboBox ivCbPorts;
+        private System.Windows.Forms.Button ivBtnConnect;
+        private System.Windows.Forms.Button ivBtnDisconnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ivBtnCAN;
     }
 }
 
